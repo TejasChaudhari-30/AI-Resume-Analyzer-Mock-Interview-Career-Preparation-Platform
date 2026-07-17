bd87a4ba77a532dd21ec4448d1845a4d1e953fa5
Tejas Chaudhari <tejaschaudhari376@gmail.com>
2026-07-18 00:21:38 +0530

    Add fornt end url

--- backend/src/main.js
+++ backend/src/main.js
@@ -16,7 +16,10 @@ const app=express();
 
 app.use(
     cors({
-        origin:"http://localhost:5173",
+        origin: [
+    "http://localhost:5173",
+    "https://ai-resume-analyzer-mock-interview-c-ten.vercel.app"
+  ],
 
         credentials:true,
     })
