2ef272b6aec8136453f0dd05acbf4c39c45662f9
Tejas Chaudhari <tejaschaudhari376@gmail.com>
2026-08-15 00:13:22 +0530

    added forgot password option port error

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -1,15 +1,22 @@
 import nodemailer from "nodemailer";
-import dotenv from "dotenv";
+import dotenv from "dotenv"
 dotenv.config();
 
-
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD
-    }
+    },
+
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 15000
 });
+
 
 export const sendEmail = async (to, subject, html) => {
 
